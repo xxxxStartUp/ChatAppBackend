2a7a5f1b419206e5de3eee114ed358ff5c4969da
somtobuchi <somtobuchi@winexviv.com>
2020-12-04 20:47:54 +0100

    redbdering and index view for landing page, adding of /user route to test middleware

--- server.js
+++ server.js
@@ -5,10 +5,25 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
+const setUser = require("./app/middleware/setCurrentUser");
+const isLoggedIn = require("./app/middleware/isAuthenticated");
+const fs = require('fs');
 //Set static folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware
+app.use(setUser);
+
+// Render a landing server page
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + '/public/views/index.html', 'utf8', (err, text) => {
+        res.send(text);
+    });
+});
+// This should return unauthorized because the setUser middleware created no object of user in the request stack
+app.get("/users", isLoggedIn, function(req, res) {
+    // get users...
+  });
 //Run when client connects
 io.on('connection', socket=>{
 
